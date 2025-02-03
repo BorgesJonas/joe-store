97acac21a66190a707b39979d3dfa6bf7d68678f
jonassborges <jonas.borges.js@gmail.com>
2025-02-03 09:58:14 -0300

    feat: created project sample and added shadcn components library

---
+++ lib/consts/index.ts
@@ -0,0 +1,7 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Joe Store";
+
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_DESCRIPTION || "Store created with Next JS";
+
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
