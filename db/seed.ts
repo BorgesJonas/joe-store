8466adf1514c987eafab8924c0bf8fc3702a1d81
jonassborges <jonas.borges.js@gmail.com>
2025-02-19 16:55:18 -0300

    feat: added next auth to the project and created added configuration file

--- db/seed.ts
+++ db/seed.ts
@@ -1,10 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 import { sampleData } from "./sample-data";
 
+/**
+ * In order to insert it you can run
+ * npx tsx ./db/seed
+ */
 async function main() {
   const prisma = new PrismaClient();
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+
   await prisma.product.createMany({ data: sampleData.products });
+  await prisma.user.createMany({ data: sampleData.users });
+
   console.log("Database seeded successufully!");
 }
 
