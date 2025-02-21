074372d28559a34aa3ecc23c522ad73b4e4d38ae
jonassborges <jonas.borges.js@gmail.com>
2025-02-21 17:21:08 -0300

    fix: removed unused user icon

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -3,5 +3,6 @@
 import { CartItem } from "@/@types";
 
 export async function addItemToCart(data: CartItem) {
+  console.log("DATA", data);
   return { success: false, message: "There was one issue" };
 }
